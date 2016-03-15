5f4b60c049097b7959e864456d284ae5b165c004
Brandon Hammel <bhammel@umail.ucsb.edu>
2016-03-15 14:18:52 -0700

    Improved page loading speed even with infinite timeout; added methods for generating certificates and private keys and for encrypting/decrpyting messages using them.

--- mproxy.py
+++ mproxy.py
@@ -9,6 +9,11 @@ import ssl
 import sys
 import threading
 
+from M2Crypto import RSA, X509
+from OpenSSL import crypto, SSL
+from pprint import pprint
+from time import gmtime, mktime
+
 
 MAXCONN = 200
 BUFSIZE = 4096
@@ -25,14 +30,58 @@ def print_and_exit(status, message, level):
     sys.exit(status)
 
 
+def create_self_signed_cert(cert_file, key_file):
+    k = crypto.PKey()
+    k.generate_key(crypto.TYPE_RSA, 1024)
+
+    cert = crypto.X509()
+    cert.get_subject().C = "US"
+    cert.get_subject().ST = "California"
+    cert.get_subject().L = "Isla Vista"
+    cert.get_subject().O = "HackItUp, Inc."
+    cert.get_subject().OU = "HackItUp, Inc."
+    cert.get_subject().CN = socket.gethostname()
+    cert.set_serial_number(1000)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(10 * 365 * 24 * 60 * 60)
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(k)
+    cert.sign(k, 'sha1')
+
+    open(cert_file, "wt").write(
+        crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+    open(key_file, "wt").write(
+        crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+
+
+def use_cert(cert_file, key_file, message):
+    f = open(cert_file)
+    cert_buffer = f.read()
+    f.close()
+
+    cert = X509.load_cert_string(cert_buffer, X509.FORMAT_PEM)
+    pub_key = cert.get_pubkey()
+    rsa_key = pub_key.get_rsa()
+    cipher = rsa_key.public_encrypt(message, RSA.pkcs1_padding)
+
+    logger.debug("Message encrypted")
+
+    read_rsa = RSA.load_key(key_file)
+    try:
+        plaintext = read_rsa.private_decrypt(cipher, RSA.pkcs1_padding)
+    except:
+        print("Error: wrong key?")
+        plaintext = ""
+
+    logger.debug("Message decrypted:")
+    logger.debug("%s", plaintext)
+
+
 class Proxy:
 
     def __init__(self, port, thread_count, timeout, logdir):
         self.timeout = timeout
         self.logdir = logdir
-        self.log = False
-        if not logdir is None:
-            self.log = True
         self.index = 0
         self.index_lock = threading.Lock()
         self.queue = Queue.Queue()
@@ -47,7 +96,7 @@ class Proxy:
             self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.sock.bind(('', port))
             self.sock.listen(MAXCONN)
-            logger.info("Proxy server listening on port %s", str(port))
+            logger.info("Proxy server listening on port %d", port)
         except Exception as ex:
             print_and_exit(1, "Unable to initialize socket", logging.ERROR)
 
@@ -67,8 +116,8 @@ class Proxy:
                 if self.timeout > 0:
                     conn.settimeout(self.timeout)
                 try:
-                    ready_socks = select.select([conn], [], [], 1)
-                    if ready_socks[0]:
+                    r, w, e = select.select([conn], [], [], 1)
+                    if conn in r:
                         data = conn.recv(BUFSIZE)
                     else:
                         conn.close()
@@ -90,7 +139,7 @@ class Proxy:
         logger.info("Port: %d", port)
         logger.info("Address: %s\n", addr[0])
         filename = ''
-        if self.log:
+        if self.logdir:
             self.index_lock.acquire()
             try:
                 filename = self.logdir + str(self.index) + "_" + str(addr[0]) + "_" + str(host)
@@ -102,20 +151,30 @@ class Proxy:
             s.connect((host, port))
             if self.timeout > 0:
                 s.settimeout(self.timeout)
-            s.send(data)
-            if self.log:
+            r, w, e = select.select([], [s], [], 1)
+            if s in w:
+                s.send(data)
+            else:
+                s.close()
+                conn.close()
+                return
+            if self.logdir:
                 with open(filename, "a+") as f:
                     f.write(data + "\n")
             while True:
                 try:
-                    ready_socks = select.select([s], [], [], 1)
-                    if ready_socks[0]:
+                    r, w, e = select.select([s], [], [], 1)
+                    if s in r:
                         response = s.recv(BUFSIZE)
                     else:
                         break
                     if len(response) > 0:
-                        conn.send(response)
-                        if self.log:
+                        r, w, e = select.select([], [conn], [], 1)
+                        if conn in w:
+                            conn.send(response)
+                        else:
+                            break
+                        if self.logdir:
                             with open(filename, "a+") as f:
                                 f.write(response)
                         logger.info("Request processed for %s (%s)", host, addr[0])
@@ -193,7 +252,7 @@ def main():
     elif args.timeout < -1:
         print_and_exit(4, "Invalid timeout", logging.ERROR)
     logdir = args.log
-    if not logdir is None:
+    if logdir:
         if not os.path.exists(logdir):
             try:
                 os.makedirs(logdir)
@@ -205,10 +264,14 @@ def main():
     logger.debug("Port: %d", args.port)
     logger.debug("Numworkers: %d", args.numworker)
     logger.debug("Timeout: %d", args.timeout)
-    if not logdir is None:
+    if logdir:
         logger.debug("Logdir: %s\n", logdir)
     else:
         logger.debug("Logdir: None\n")
+    cert_file = "selfsigned.crt"
+    key_file = "private.key"
+    create_self_signed_cert(cert_file, key_file)
+    use_cert(cert_file, key_file, "Fuck you!")
     proxy = Proxy(args.port, args.numworker, args.timeout, logdir)
     proxy.start()
 
